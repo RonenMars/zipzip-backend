cbae0ba8c4160379f06ff2be9030fdfdfa2570e7
RonenMars <cleanclaw@gmail.com>
2023-08-28 10:15:26 +0300

    Refactor

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -12,12 +12,7 @@ import { AccountService } from './account.service';
 import { JoiValidationPipe } from '@validations/joi-schema.validation';
 import { PhoneSchema, UserSchema } from '@validations/user';
 import { UserCreateDto } from '@validations/user/dto';
-import {
-  generateOTP,
-  getPhoneNumber,
-  hashPassword,
-  isValidPhoneNumber,
-} from '@root/utils';
+import { generateOTP, getPhoneNumber, hashPassword } from '@root/utils';
 import * as moment from 'moment';
 import { TwilioService } from 'nestjs-twilio';
 import { ConfigService } from '@nestjs/config';
@@ -36,44 +31,39 @@ export class AccountController {
   @Post()
   @UsePipes(new JoiValidationPipe(UserSchema))
   async createUser(@Body() user: UserCreateDto) {
-    if (isValidPhoneNumber(user.phone)) {
-      user.codeExpiration = moment().format();
-      user.validationCode = generateOTP(4);
+    user.codeExpiration = moment().format();
+    user.validationCode = generateOTP(4);
 
-      await this.accountService.createUser(user);
-    }
+    await this.accountService.createUser(user);
   }
 
   @Get(':phone')
   @UsePipes(new JoiValidationPipe(PhoneSchema))
   async loginUser(@Param('phone') phone: string) {
-    if (isValidPhoneNumber(phone)) {
-      const phoneNumber = getPhoneNumber(phone);
-      const user = await this.accountService.user(phoneNumber);
-      const otpForUser = generateOTP(OTP_LENGTH);
-      const message = `Your code is ${otpForUser}`;
+    const phoneNumber = getPhoneNumber(phone);
+    const user = await this.accountService.user(phoneNumber);
+    const otpForUser = generateOTP(OTP_LENGTH);
+    const message = `Your code is ${otpForUser}`;
 
-      if (user) {
-        await this.accountService.updateUser({
-          where: { phone: user.phone },
-          data: {
-            ...user,
-            validationCode: await hashPassword(otpForUser),
-            codeExpiration: moment().add(3, 'minutes').format(),
-          },
-        });
-        await sendSMS(
-          <string>this.configService.get(EnvVariables.TwilioSenderPhoneNumber),
-          user.phone,
-          message,
-          this.twilioService.client,
-        );
+    if (user) {
+      await this.accountService.updateUser({
+        where: { phone: user.phone },
+        data: {
+          ...user,
+          validationCode: await hashPassword(otpForUser),
+          codeExpiration: moment().add(3, 'minutes').format(),
+        },
+      });
+      await sendSMS(
+        <string>this.configService.get(EnvVariables.TwilioSenderPhoneNumber),
+        user.phone,
+        message,
+        this.twilioService.client,
+      );
 
-        return { userId: user.id };
-      } else {
-        throw new HttpException('No users found', HttpStatus.NO_CONTENT);
-      }
+      return { userId: user.id };
+    } else {
+      throw new HttpException('No users found', HttpStatus.NO_CONTENT);
     }
-    throw new HttpException('Bad phone number', HttpStatus.BAD_REQUEST);
   }
 }
